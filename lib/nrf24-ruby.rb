d81f1f34ff76ebfc422a3f2c29a6ddbe0cba2dfa
Eflukx <rogier@lodewijks.org>
2016-04-21 15:59:41 +0200

    Changed to require_relative

--- lib/nrf24-ruby.rb
+++ lib/nrf24-ruby.rb
@@ -1,4 +1,4 @@
-require 'nrf24/version'
-require 'nrf24/constants'
-require 'nrf24/nrf24-ruby'
-require 'nrf24/bcm2835_driver'
+require_relative 'nrf24/version'
+require_relative 'nrf24/constants'
+require_relative 'nrf24/nrf24-ruby'
+require_relative 'nrf24/bcm2835_driver'
